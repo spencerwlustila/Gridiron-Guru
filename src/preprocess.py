b8f2500929f99b568ac97e6d2820bf57c840f551
Spencer Lustila <spencerwlustila@gmail.com>
2025-08-25 10:18:10 -0400

    Fixed the merging of the datasets into one and wrote it to player_stats.csv in data/processed

--- src/preprocess.py
+++ src/preprocess.py
@@ -16,9 +16,34 @@ receiving = receiving.drop(columns=["-9999"])
 
 
 
-passing = passing[['Player', 'Pos', 'Yds', 'TD', 'Int']]
-rushing = rushing[['Player','Pos','Yds','TD']]
-receiving = receiving[['Player','Pos','Rec','Yds','TD']]
+passing = passing[['Player','Pos', 'Yds', 'TD', 'Int']].rename(
+    columns={'Yds': 'PassYds', 'TD': 'PassTDs'})
+
+rushing = rushing[['Player','Pos', 'Yds', 'TD']].rename(
+    columns={'Yds':'RushYds', 'TD':'RushTDs'})
+
+receiving = receiving[['Player','Pos','Rec','Yds','TD']].rename(
+    columns={'Yds': 'RecYds', 'TD':'RecTDs'})
+
+#use merge instead of concat to have one row for each player showing all stats
+player_stats = (
+    passing
+    .merge(rushing, on=["Player","Pos"], how="outer")
+    .merge(receiving, on=["Player","Pos"], how="outer")
+)
+
+#replace NaN with 0 for looks
+player_stats.fillna(0, inplace=True)
+
+pd.set_option("display.max_columns", None)   # show all columns
+pd.set_option("display.width", None)         # don't wrap columns
+pd.set_option("display.max_colwidth", None)  # show full column
+pd.set_option("display.max_rows", None)  #show all rows
+
+print(player_stats)
+
+player_stats.to_csv("../data/processed/player_stats.csv")
+
 
 
 
